22f688712815dba481f87fdf90a4a9714fb752b1
ErhanBatu <e.batu44@icloud.com>
2023-04-15 12:11:58 +0200

    Add @autowired and @controller

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,14 +1,17 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
     //I initiate this class if I pass greetingservice
+    //because we have here const, spring will automatically autowired it, you don't have to put @autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -1,16 +1,23 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt, Spring Framework Guru.
  */
+@Controller
 public class SetterInjectedController {
 
     //so here I am doing without spring dependency injection
+    //either you can autowired property or setter
     private GreetingService greetingService;
 
+    //i want here my setter should be visible and inject it on the console and utilized.
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
+        System.out.println("I set greeting service");
         this.greetingService = greetingService;
     }
 
