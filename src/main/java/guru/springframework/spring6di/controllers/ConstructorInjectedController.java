c0a712c5f6ac6ac50a39bdd4e0d50a22b6718857
Erhan <e.batu44@icloud.com>
2023-06-07 01:05:59 +0200

    35. Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
-//If you don't want to use Sprinf annotation then you should use this method
+//If you don't want to use Spring annotation then you should use this method
+@Controller
 public class ConstructorInjectedController {
 
     //Because this is const I make it final
--- src/test/java/guru/springframework/spring6di/controllers/MycontrollerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/MycontrollerTest.java
@@ -1,11 +1,12 @@
 package guru.springframework.spring6di.controllers;
 
+import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MycontrollerTest {
-
     @Test
     void sayHello() {
     }
